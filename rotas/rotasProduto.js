4080091a8edadc4e4d73475d0dc4cb79e8e2b67b
RuanFigueiredo <ruanthiago67@gmail.com>
2022-08-19 10:38:35 -0300

    add nodemon

--- rotas/rotasProduto.js
+++ rotas/rotasProduto.js
@@ -4,7 +4,7 @@ const ProdutoController = require('../controllers/ProdutoController')
 const router = express.Router();
 
 router.get('/criar', ProdutoController.criarProduto);
-//router.get('/deletar', ProdutoController.deletarProduto);
+//router.get('/deletar', ProdutoController.deletarProduto); 
 
 
-module.exports = router;
+module.exports = router;        
