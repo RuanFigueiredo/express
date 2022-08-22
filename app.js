03493cafc33350f2bf0a83c524adcaa10bbaee2b
RuanFigueiredo <ruanthiago67@gmail.com>
2022-08-22 08:59:58 -0300

    consertando erro de rota

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //importando o express
 const express = require('express');
-const res = require('express/lib/response');
+
 //const req = require('express/lib/request');
 
 let app = express();
@@ -12,8 +12,8 @@ app.get('/contatos', (req,res)=>res.send("página de contatos"));
 
 //rotas parametrizadas ou rotas dinâmicas
 app.get('/produtos/:id', (req,res)=>{
-    let {id} = req.params
-    res.send("eu tenho um produto com o id:", id)
+    let {id} = req.params;
+    res.send("eu tenho o produto com o id: " +   id)
 });
 
 //criando servidor usando express   
