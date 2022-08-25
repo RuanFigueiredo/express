040f8bc5e127f26b5414a6209fcb0f8596090dfb
RuanFigueiredo <ruanthiago67@gmail.com>
2022-08-25 15:46:06 -0300

    tirando comentário

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 //importando o express
 const express = require('express');
-
-//const req = require('express/lib/request');
-
 let app = express();
 
 
@@ -10,12 +7,12 @@ let app = express();
 app.get('/', (req,res)=>res.send("Olá mundo"));
 app.get('/contatos', (req,res)=>res.send("página de contatos"));
 
-//rotas parametrizadas ou rotas dinâmicas
+
 app.get('/produtos/:id?', (req,res)=>{
     let {id} = req.params;
     res.send("eu tenho o produto com o id: " +   id)
 });
 
-//criando servidor usando express   
+  
 app.listen(2000,()=> console.log("servidor rodando na porta 2000...")
 )
