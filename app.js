36998d0e5c6f22d41b430213faf44ba588db82cc
RuanFigueiredo <ruanthiago67@gmail.com>
2022-08-25 16:56:41 -0300

    adicionando um sistema de roteamento com método use

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 //importando o express
 const express = require('express');
+const rotasProdutos = require('./rotas/rotasProdutos'); 
 let app = express();
 
 
 //routing response ou resposta da rota
 app.get('/', (req,res)=>res.send("Olá mundo"));
-app.get('/contatos', (req,res)=>res.send("página de contatos"));
+//app.get('/contatos', (req,res)=>res.send("página de contatos"));
 
+app.use('/produtos', rotasProdutos);
 
-app.get('/produtos/:id?', (req,res)=>{
+/*app.get('/produtos/:id?', (req,res)=>{
     let {id} = req.params;
     res.send("eu tenho o produto com o id: " +   id)
-});
+});*/
 
   
 app.listen(2000,()=> console.log("servidor rodando na porta 2000...")
